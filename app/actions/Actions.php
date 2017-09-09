ad036170d549f3d9dc44fe8b3aa5651dc2699580
Dmitry <korobko.dmitry@ratkussoft.com>
2017-09-09 16:30:35 +0300

    Final corrections

--- app/actions/Actions.php
+++ app/actions/Actions.php
@@ -16,19 +16,14 @@ class Actions {
     public function getParse(){
         echo "Please enter URL for parsing: \n";
         $url = readline();
-        $domain = $this->getDomain($url);
-        $linksArray = $this->getUrlLinksOfDomain($url); var_dump($linksArray); exit;
-        $imagesArray = $this->getImageLinks($linksArray);
-        $this->readWriteReport($domain, true, $imagesArray);
+        $this->getImagesLinksFromAllLinksOfDomainByUrl($url);
     }
 
     // Report result of domain analyse function
     public function getReport() {
         echo "Please enter domain for analyse: \n";
         $url = readline();
-        $domain = $this->getDomain($url);
-        $report = $this->readWriteReport($domain, false);
-        print_r($report);
+        $this->getImagesLinksFromAllLinksOfDomainByUrl($url, false);
     }
 
     // Help view function
@@ -41,14 +36,14 @@ class Actions {
         echo "3. help            - get help with list of commands\n";
     }
 
-    //Search all links of domain in the pages by URL function
-    public function getUrlLinksOfDomain($url, $urls = null) {
+    //Search all images links from all links of domain in the pages by URL function
+    public function getImagesLinksFromAllLinksOfDomainByUrl($url, $forWrite = true, $urls = null) {
         $from = (substr($url, 0, 4) == 'http') ? $url : 'http://' . $url;
         $domain = $this->getDomain($from);
         $allLinkIsChecked = true;
         $urls[$from] = true;
 
-        if (@fopen($from, "r")) {
+        if (@fopen($from, "r") && @file_get_contents($from)) {
             $html = file_get_contents($from);
             preg_match_all("/<[Aa][\s]{1}[^>]*[Hh][Rr][Ee][Ff][^=]*=[ '\"\s]*([^ \"'>\s#]+)[^>]*>/", $html, $matches);
 
@@ -61,23 +56,59 @@ class Actions {
             }
 
             foreach ($urls as $key => $value) {
-                if ($value === false) {
+
+                if (!$value) {
                     $allLinkIsChecked = false;
                 }
             }
 
-            if ($allLinkIsChecked === false) {
+            if (!$allLinkIsChecked) {
                 $urlsArray = [];
 
                 foreach ($urls as $key => $value) {
-                    if ($value === false) {
-                        $urlsArray = $urlsArray + ($this->getUrlLinksOfDomain($key, $urls));
+
+                    if (!$value) {
+                        $urls[$key] = true;
+
+                        if ($forWrite) {
+                            $urlsArray += ($this->getImagesLinksFromAllLinksOfDomainByUrl($key, true, $urls));
+                        } else {
+                            $urlsArray += ($this->getImagesLinksFromAllLinksOfDomainByUrl($key,false, $urls));
+                        }
                     }
                 }
 
-                $urls = $urls + $urlsArray;
+                $urls += $urlsArray;
+                $allLinkIsChecked = true;
+
+                foreach ($urls as $key => $value) {
+
+                    if (!$value) {
+                        $allLinkIsChecked = false;
+                    }
+                }
+
+                if ($allLinkIsChecked) {
+
+                    if ($forWrite) {
+                        $this->readWriteReport($domain, true, $this->getImagesLinks($urls));
+                    } else {
+                        $this->readWriteReport($domain, false, $this->getImagesLinks($urls));
+                    }
+
+                    exit();
+                }
 
                 return $urls;
+            } else {
+
+                if ($forWrite) {
+                    $this->readWriteReport($domain, true, $this->getImagesLinks($urls));
+                } else {
+                    $this->readWriteReport($domain, false, $this->getImagesLinks($urls));
+                }
+
+                exit();
             }
         }
 
@@ -85,28 +116,39 @@ class Actions {
     }
 
     //Search all images in the pages by URL function
-    public function getImageLinks($urlsArray)
+    public function getImagesLinks($urlsArray)
     {
-        $imageLinks = [];
+        $imageLinksWithSourcePages = [];
 
         foreach ($urlsArray as $urlKey => $urlValue) {
-            $html = file_get_contents($urlKey);
-            preg_match_all("/<[Ii][Mm][Gg][\s]{1}[^>]*[Ss][Rr][Cc][^=]*=[ '\"\s]*([^ \"'>\s#]+)[^>]*>/", $html, $matches);
 
-            foreach ($matches[1] as $key => $value){
-                if (substr($value, 0, 4) == 'http') {
-                    if(@fopen($value, "r")) {
-                        $imageLinks[$key] = 'link: ' . $value . ' source page: ' . $key;
+            if (@fopen($urlKey, "r") && @file_get_contents($urlKey)) {
+                $html = file_get_contents($urlKey);
+                preg_match_all("/<[Ii][Mm][Gg][\s]{1}[^>]*[Ss][Rr][Cc][^=]*=[ '\"\s]*([^ \"'>\s#]+)[^>]*>/", $html, $matches);
+                $imageLinks = [];
+
+                foreach ($matches[1] as $key => $value) {
+
+                    if (substr($value, 0, 4) == 'http') {
+
+                        if (@fopen($value, "r")) {
+                            $imageLinks[$key] = $value;
+                        }
+                    } else {
+
+                        if (@fopen($urlKey . $value, "r")) {
+                            $imageLinks[$key] = $urlKey . $value;
+                        }
                     }
-                } else {
-                    if(@fopen($urlKey . $value, "r")) {
-                        $imageLinks[$key] = 'link: ' . $urlKey . $value . ' source page: ' . $key;
-                    }
+                }
+
+                if (!empty($imageLinks)) {
+                    $imageLinksWithSourcePages['Source page: ' . $urlKey] = $imageLinks;
                 }
             }
         }
 
-        return array_unique($imageLinks);
+        return (!empty($imageLinksWithSourcePages)) ? $imageLinksWithSourcePages : 'Do not have any images by this URL';
     }
 
     //Get domain by URL function
@@ -121,37 +163,51 @@ class Actions {
     public function readWriteReport($domain, $forWrite = true, $dataArray = null) {
 
         if ($forWrite) {
+
             if ($dataArray !== null) {
                 $report = fopen(__DIR__ . '/../reports/' . $domain . '.csv', 'w');
 
                 foreach ($dataArray as $item) {
-                    fputcsv($report, $item);
+                    fputcsv($report, $item , ';', '"');
                 }
 
                 fclose($report);
-            } else {
 
-            } return 'Not images by this URL';
+                echo 'Link to results of analyse: ' . "\n" . realpath(__DIR__ . "/../reports/" . $domain . '.csv') . "\n";
+            } else {
+                echo 'Not images by this URL';
+            }
         } else {
+              // If we want read results of domain analyse from already existing file
+//            if (@fopen(__DIR__ . '/../reports/' . $domain . '.csv', 'r')) {
+//                if (($report = fopen(__DIR__ . '/../reports/' . $domain . '.csv', 'r')) !== false) {
+//                    $reportArray = [];
+//
+//                    while (($data = fgetcsv($report, 1000, ";")) !== false) {
+//                        $num = count($data);
+//
+//                        for ($i = 0; $i < $num; $i++) {
+//                            $reportArray[] = $data[$i];
+//                        }
+//                    }
+//
+//                    fclose($report);
+//
+//                    return $reportArray;
+//
+//                }
+//            } else {
+//
+//                echo 'Error of open file';
+//            }
 
-            if (($report = fopen(__DIR__ . '/../reports/' . $domain . '.csv', 'r')) !== false) {
-                $reportArray = [];
-
-                while (($data = fgetcsv($report, 1000, ",")) !== false) {
-                    $num = count($data);
-
-                    for ($i=0; $i < $num; $i++) {
-                        $reportArray[] =  $data[$i];
-                    }
+            //If we want see results of domain analyse without creating file
+            echo 'Results of analyse for domain: ' . $domain . "\n";
+            foreach ($dataArray as $dataItem => $dataValue) {
+                echo "\n" . $dataItem . ": \n Links: \n";
+                foreach ($dataArray[$dataItem] as $value) {
+                    echo  $value . "\n";
                 }
-
-                fclose($report);
-
-                return $reportArray;
-
-            } else {
-
-                return 'Error of open file';
             }
         }
     }
